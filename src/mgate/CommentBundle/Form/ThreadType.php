80ba77e460ea12a47cdd82e0eef74a5957fc5c23
ThÃ©o FIDRY <theo.fidry@gmail.com>
2015-06-24 10:40:26 +0200

    Fixed some Insight issues

--- src/mgate/CommentBundle/Form/ThreadType.php
+++ src/mgate/CommentBundle/Form/ThreadType.php
@@ -14,7 +14,6 @@ namespace mgate\CommentBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class ThreadType extends AbstractType
 {
