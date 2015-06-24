000ea2378f1760936a9a4920e0b4584e0b340fb1
ThÃ©o FIDRY <theo.fidry@gmail.com>
2015-06-24 12:07:19 +0200

    Removed psysh to use PsyshBundle instead

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -74,6 +74,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Fidry\PsyshBundle\PsyshBundle();
         }
 
         return $bundles;
