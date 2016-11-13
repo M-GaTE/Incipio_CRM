2dbd44b6cb0629252a90726e259a2bf65bad9635
stoakes <Stoakes@users.noreply.github.com>
2016-11-13 16:24:06 +0100

    Remove custom configuration loader
    This extension could be useful if there was some configuration in app/config/config_*.yml for those bundles.
    As long as there aren't, I remove them.

--- src/mgate/CommentBundle/DependencyInjection/Configuration.php
+++ src/mgate/CommentBundle/DependencyInjection/Configuration.php
@@ -27,8 +27,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mgate_comment');
-
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
