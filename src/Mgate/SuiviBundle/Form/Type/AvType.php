2680bd53ce0ee36c7ed06cc625d7599fa4a9a54c
stoakes <Stoakes@users.noreply.github.com>
2017-01-28 12:55:18 +0100

    Fix bugs on AvType :
     - prospect employee not displayed
     - conflict on clausechoice

--- src/Mgate/SuiviBundle/Form/Type/AvType.php
+++ src/Mgate/SuiviBundle/Form/Type/AvType.php
@@ -28,9 +28,9 @@ class AvType extends DocTypeType
         détailler l\'historique des relations avec le client et du travail sur l\'étude qui ont conduit à l\'Avenant.',
         'required' => true, ))
         ->add('clauses', ChoiceType::class, array('label' => 'Type d\'avenant', 'multiple' => true, 'choices' => Av::getClausesChoices(),
-            'choice_label' => function ($var) {
-                return $var;
-            }, ))
+            'choice_label' => function ($val, $key) {
+                return $key;
+            },))
         ->add('phases', CollectionType::class, array(
                 'entry_type' => PhaseType::class,
                 'entry_options' => array('isAvenant' => true),
