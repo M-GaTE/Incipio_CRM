7910731b2db7052875f5cd562d9bd0bf15a24e89
stoakes <Stoakes@users.noreply.github.com>
2017-01-28 11:02:37 +0100

    Use paramconverter in ApController

--- src/Mgate/SuiviBundle/Controller/ApController.php
+++ src/Mgate/SuiviBundle/Controller/ApController.php
@@ -12,6 +12,7 @@
 namespace Mgate\SuiviBundle\Controller;
 
 use Mgate\SuiviBundle\Entity\Ap;
+use Mgate\SuiviBundle\Entity\Etude;
 use Mgate\SuiviBundle\Form\Type\ApType;
 use Mgate\SuiviBundle\Form\Type\DocTypeSuiviType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -24,31 +25,26 @@ class ApController extends Controller
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
      */
-    public function indexAction($page)
+    public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('MgateSuiviBundle:Etude')->findAll();
 
         return $this->render('MgateSuiviBundle:Etude:index.html.twig', array(
-                    'etudes' => $entities,
-                ));
+            'etudes' => $entities,
+        ));
     }
 
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
+     * @param Etude $etude
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function voirAction($id)
+    public function voirAction(Etude $etude)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        //attention reflechir si faut passer id etude ou rester en id Ap
-        // en fait y a 2 fonction voir
-        // une pour voir le suivi
-        // et une pour voir la redaction
-        $etude = $em->getRepository('MgateSuiviBundle:Etude')->find($id);
-        $entity = $etude->getAp();
-        if (!$entity) {
+        $ap = $etude->getAp();
+        if (!$ap) {
             throw $this->createNotFoundException('L\'Avant-Projet demandé n\'existe pas !');
         }
 
@@ -57,20 +53,19 @@ class ApController extends Controller
         }
 
         return $this->render('MgateSuiviBundle:Ap:voir.html.twig', array(
-                    'ap' => $entity,
-                ));
+            'ap' => $ap,
+        ));
     }
 
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
+     * @param Request $request
+     * @param Etude $etude etude which Ap should be edited
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function redigerAction(Request $request, $id)
+    public function redigerAction(Request $request, Etude $etude)
     {
         $em = $this->getDoctrine()->getManager();
-
-        if (!$etude = $em->getRepository('Mgate\SuiviBundle\Entity\Etude')->find($id)) {
-            throw $this->createNotFoundException('L\'étude demandée n\'existe pas!');
-        }
 
         if ($this->get('Mgate.etude_manager')->confidentielRefus($etude, $this->getUser(), $this->get('security.authorization_checker'))) {
             throw new AccessDeniedException('Cette étude est confidentielle');
@@ -100,21 +95,19 @@ class ApController extends Controller
         }
 
         return $this->render('MgateSuiviBundle:Ap:rediger.html.twig', array(
-                    'form' => $form->createView(),
-                    'etude' => $etude,
-                ));
+            'form' => $form->createView(),
+            'etude' => $etude,
+        ));
     }
 
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
+     * @param Request $request
+     * @param Etude $etude
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function suiviAction(Request $request, $id)
+    public function suiviAction(Request $request, Etude $etude)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        if (!$etude = $em->getRepository('Mgate\SuiviBundle\Entity\Etude')->find($id)) {
-            throw $this->createNotFoundException('L\'étude demandée n\'existe pas!');
-        }
 
         if ($this->get('Mgate.etude_manager')->confidentielRefus($etude, $this->getUser(), $this->get('security.authorization_checker'))) {
             throw new AccessDeniedException('Cette étude est confidentielle');
@@ -134,8 +127,8 @@ class ApController extends Controller
         }
 
         return $this->render('MgateSuiviBundle:Ap:modifier.html.twig', array(
-                    'form' => $form->createView(),
-                    'etude' => $etude,
-                ));
+            'form' => $form->createView(),
+            'etude' => $etude,
+        ));
     }
 }
