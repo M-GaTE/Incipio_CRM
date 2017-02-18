77906e13d3eb9a7c1d38752113e4bc15729141e1
stoakes <Stoakes@users.noreply.github.com>
2017-02-18 17:30:37 +0100

    Restrict app_dev.php access to localhost

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,6 +7,13 @@ use Symfony\Component\HttpFoundation\Request;
 //umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
 
 $loader = require __DIR__.'/../app/autoload.php';
 
