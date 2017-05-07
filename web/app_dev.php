6bab63a9f57dde2bce346a752a4d1b08a14a69be
stoakes <Stoakes@users.noreply.github.com>
2017-05-07 15:54:23 +0200

    Use env var to restrict access to app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || getenv('SYMFONY_ENV') != 'dev'
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
